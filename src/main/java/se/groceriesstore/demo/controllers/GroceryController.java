3f7c4b9c8b9d94e3b43950e1ba09254962ca8689
n1cken <nicken333@gmail.com>
2021-04-07 16:42:22 +0200

    updated quantity functions

--- src/main/java/se/groceriesstore/demo/controllers/GroceryController.java
+++ src/main/java/se/groceriesstore/demo/controllers/GroceryController.java
@@ -16,8 +16,13 @@ public class GroceryController {
     @Autowired
     DrinkService drinkService;
 
-    @RequestMapping("/index")
-    public String start(){
+    @RequestMapping("index")
+    public String start(Model model){
+        model.addAttribute("products", productService.getProducts());
+        model.addAttribute("breads", productService.getAllBreads());
+        model.addAttribute("drinks", productService.getAllDrinks());
+        model.addAttribute("fruits", productService.getAllFruits());
+        model.addAttribute("vegetables", productService.getAllVegetables());
         return "index";
     }
 
--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -1,24 +1,44 @@
 let productQuantity = 0;
+let thisQuantity = 0;
+
+$(function() {
+
+$('.plus-button').on('click', function () {
+    number = $(this).closest('div').find('.product-quantity').val();
+    number++;
+    $(this).closest('div').find('.product-quantity').text(number);
+})
+
+
+$('.minus-button').on('click', function () {
+    number = $(this).closest('div').find('.product-quantity').val();
+    number--;
+    $(this).closest('div').find('.product-quantity').text(number);
+})
+})
+
 
 function increaseQuantity() {
-    productQuantity += 1;
-    $('#quantity').text(productQuantity);
+    //$(this) pekade på funktionen och kunde därmed
+    //ej nå DOMens element. Därav gjort om struktur
+    //efter så som läraren gjort.
+
+      // productQuantity += 1;
+     //  $(this).closest('.card-body').find('.product-quantity').text(productQuantity);
+     //  console.log($(this));
+    // $('#quantity').text(productQuantity);
     checkIfMoreThanZero(productQuantity);
 }
 
 function decreaseQuantity() {
-    if (productQuantity > 0) {
-        productQuantity -= 1;
-        $('#quantity').text(productQuantity);
-    }
+    checkIfMoreThanZero(productQuantity);
 }
 
-    function checkIfMoreThanZero(q) {
-        if (q > 0) {
-            $('#cartcard').removeClass('notActive');
-        }
-        else if (q <= 0) {
-            $('#cartcard').addClass('notActive');
-        }
-
+function checkIfMoreThanZero(q) {
+    if (q >= 0) {
+        $('#cartcard').removeClass('notActive');
+    }
+    else if (q < 1) {
+        $('#cartcard').addClass('notActive');
+    }
 }
