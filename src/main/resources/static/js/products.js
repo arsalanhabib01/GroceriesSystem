5d1c0262bb55824e61aa24c09512219e9110a6c7
nicholas <nicken333@gmail.com>
2021-04-07 10:57:43 +0200

    cart shows when productQuanity is more than zero

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -3,11 +3,19 @@ let productQuantity = 0;
 function increaseQuantity() {
     productQuantity += 1;
     $('#quantity').text(productQuantity);
+    checkIfMoreThanZero(productQuantity);
 }
 
 function decreaseQuantity() {
-    if(productQuantity > 0) {
+    if (productQuantity > 0) {
         productQuantity -= 1;
         $('#quantity').text(productQuantity);
     }
+}
+
+    function checkIfMoreThanZero(q) {
+        if (q > 0) {
+            $('#cartcard').removeClass('notActive');
+        }
+
 }
