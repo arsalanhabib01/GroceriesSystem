df578aba30f006370f617e281e498372b9084cbc
n1cken <nicken333@gmail.com>
2021-04-08 14:14:30 +0200

    Juices added cart and new amount choice, console log name of product

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -1,9 +1,36 @@
 let productQuantity = 0;
-let thisQuantity = 0;
 
 $(document).ready(function() {
 
+    $('.form-control').change(function() {
+        updateQuantity(this.value);
+        let name = $(this).closest('div').find('.card-title').text();
+        addToCart(name);
+    });
 
+})
+
+function updateQuantity(quantityInput) {
+    checkIfMoreThanZero(quantityInput);
+}
+
+function addToCart (name) {
+    console.log(name)
+}
+
+
+
+
+function checkIfMoreThanZero(q) {
+    if (q >= 0) {
+        $('#cartcard').removeClass('notActive');
+    }
+    else if (q < 1) {
+        $('#cartcard').addClass('notActive');
+    }
+}
+
+//OLD FUNCTIONS
 $('.plus-button').on('click', function () {
     number = $(this).closest('div').find('.product-quantity').val();
     number++;
@@ -17,42 +44,19 @@ $('.minus-button').on('click', function () {
     $(this).closest('div').find('.product-quantity').text(number);
 })
 
-    $('.form-control').change(function() {
-        updateQuantity(this.value);
-    });
-
-})
-
-
-
-function updateQuantity(quantityInput) {
-    /* Calculate line price */
-    console.log(quantityInput);
-    checkIfMoreThanZero(quantityInput);
-}
-
 
 function increaseQuantity() {
     //$(this) pekade på funktionen och kunde därmed
     //ej nå DOMens element. Därav gjort om struktur
     //efter så som läraren gjort.
 
-      // productQuantity += 1;
-     //  $(this).closest('.card-body').find('.product-quantity').text(productQuantity);
-     //  console.log($(this));
+    // productQuantity += 1;
+    //  $(this).closest('.card-body').find('.product-quantity').text(productQuantity);
+    //  console.log($(this));
     // $('#quantity').text(productQuantity);
 
 }
 
 function decreaseQuantity() {
     checkIfMoreThanZero(productQuantity);
-}
-
-function checkIfMoreThanZero(q) {
-    if (q >= 0) {
-        $('#cartcard').removeClass('notActive');
-    }
-    else if (q < 1) {
-        $('#cartcard').addClass('notActive');
-    }
 }
