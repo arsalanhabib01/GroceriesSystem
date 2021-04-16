3efb04cf7e080961c0cfcce44184241bad813a9d
adamkarmander <71024067+adamkarmander@users.noreply.github.com>
2021-04-16 17:16:00 +0200

    Added Test-class for ProductService class

---
+++ src/test/java/se/groceriesstore/demo/services/ProductServiceTest.java
@@ -0,0 +1,80 @@
+package se.groceriesstore.demo.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @Test
+    void getCart() {
+    }
+
+    @Test
+    void getProducts() {
+    }
+
+    @Test
+    void getAllFruits() {
+    }
+
+    @Test
+    void getAllVegetables() {
+    }
+
+    @Test
+    void getAllDrinks() {
+    }
+
+    @Test
+    void getAllBreads() {
+    }
+
+    @Test
+    void addFruit() {
+    }
+
+    @Test
+    void addVegetable() {
+    }
+
+    @Test
+    void addDrink() {
+    }
+
+    @Test
+    void addBread() {
+    }
+
+    @Test
+    void getFruitById() {
+    }
+
+    @Test
+    void getVegetableById() {
+    }
+
+    @Test
+    void getDrinkById() {
+    }
+
+    @Test
+    void getBreadById() {
+    }
+
+    @Test
+    void deleteFruit() {
+    }
+
+    @Test
+    void deleteVegetable() {
+    }
+
+    @Test
+    void deleteDrink() {
+    }
+
+    @Test
+    void deleteBread() {
+    }
+}
