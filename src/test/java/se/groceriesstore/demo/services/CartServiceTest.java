d2ba4a1b90103fde26029ddab56e62aceb26df40
nicholas <nicken333@gmail.com>
2021-04-19 08:57:29 +0200

    cart init ready

--- src/test/java/se/groceriesstore/demo/services/CartServiceTest.java
+++ src/test/java/se/groceriesstore/demo/services/CartServiceTest.java
@@ -1,14 +1,24 @@
 package se.groceriesstore.demo.services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import se.groceriesstore.demo.dao.CartDAO;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class CartServiceTest {
+
+    public static CartDAO cartDAO;
+    public CartService cartService;
+
+    @BeforeEach
+    public void init () {
+        cartService = new CartService(cartDAO);
+    }
 
     @Test
     void addCart() {
